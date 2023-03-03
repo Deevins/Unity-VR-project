9a4fbbe05c837dad7bca088ef45f7dd0d58ada92
deevins <daker255@bk.ru>
2023-03-03 23:30:36 +0300

    ended lesson 7. Now should test menu, menu button and is menu working. test for bugs

--- Assets/ActivateTeleportationRay.cs
+++ Assets/ActivateTeleportationRay.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class ActivateTeleportationRay : MonoBehaviour
 {
@@ -10,16 +11,20 @@ public class ActivateTeleportationRay : MonoBehaviour
     public InputActionProperty rightActivate;
 
     public InputActionProperty rightCancel;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    public XRRayInteractor rightRay;
 
     // Update is called once per frame
     void Update()
     {
-        rightTeleportation.SetActive(rightCancel.action.ReadValue<float>() == 0 && rightActivate.action.ReadValue<float>() > 0.1f);
+        bool isRightRayHovering = rightRay.TryGetHitInfo(
+            out Vector3 rightPos,
+            out Vector3 rightNormal,
+            out int rightNumber,
+            out bool rightValid);
+        
+        rightTeleportation.SetActive(!isRightRayHovering &&
+                                     rightCancel.action.ReadValue<float>() == 0 &&
+                                     rightActivate.action.ReadValue<float>() > 0.1f);
     }
 }
